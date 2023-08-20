7a391907a447031baab4b28e0ee4301f9ce61f2d
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2023-08-20 01:46:00 -0300

    Adds User Details Page

---
+++ app/users/[username]/page.jsx
@@ -0,0 +1,62 @@
+"use client";
+import React from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const UserDetails = ({ params }) => {
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+
+  const openProfile = () => {
+    window.open(user?.html_url, '_blank');
+  };
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch(
+        `https://lucbar-github-node-api.onrender.com/users/${params.username}/details`
+      );
+      const data = await response.json();
+      setUser(data.user);
+    };
+
+    fetchUser();
+  }, []);
+
+  return (
+    <div className="min-h-screen py-10">
+      <div className="max-w-3xl mx-auto p-6 bg-slate-700 shadow-md rounded-lg">
+        <h1 className="text-3xl font-semibold mb-4">
+          Welcome to {user?.login} Details
+        </h1>
+        <div className="flex items-center space-x-4">
+          <img
+            src={user?.avatar_url}
+            alt="User Avatar"
+            width={80}
+            height={80}
+            className="rounded-full"
+          />
+          <div>
+            <h2 className="text-xl text-gray-200 font-semibold">
+              {user?.login}
+            </h2>
+            <p className="text-gray-200">ID: {user?.id}</p>
+            <p className="text-gray-200">Profile URL: {user?.html_url}</p>
+            <p className="text-gray-200">
+              Login Creation Date: {user?.created_at}
+            </p>
+            <button
+              className="bg-black text-white py-2 font-semibold px-4 rounded-full mt-2"
+              onClick={openProfile}
+            >
+              Visit GitHub Profile
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserDetails;
--- components/UserCard.jsx
+++ components/UserCard.jsx
@@ -1,13 +1,11 @@
 "use client";
-
-import { useState } from "react";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 
 import React from "react";
 import "../styles/UserCard.css";
 
-const UserCard = ({ user, handleTagClick }) => {
+const UserCard = ({ user}) => {
   const pathName = usePathname();
   const router = useRouter();
 
@@ -26,7 +24,9 @@ const UserCard = ({ user, handleTagClick }) => {
             <h3 className="font-satoshi font-semibold text-white-900">
               {user.login}
             </h3>
-            <button className="px-5 py-1.5 text-sm bg-green-700 rounded-full text-white">
+            <button 
+            className="px-5 py-1.5 text-sm bg-green-700 rounded-full text-white"
+            onClick={() => router.push(`/users/${user.login}`)}>
               View Profile
             </button>
           </div>
